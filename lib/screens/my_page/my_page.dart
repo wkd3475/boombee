0694f36029f6f9ff81e7a415d85a8ded07c69fb0
wkd3475 <wkd3475@gmail.com>
2021-02-24 22:07:03 +0900

    add : 마이 페이지 추가

--- lib/screens/my_page/my_page.dart
+++ lib/screens/my_page/my_page.dart
@@ -1,11 +1,111 @@
 import 'package:flutter/material.dart';
 
 class MyPage extends StatelessWidget {
-  // This widget is the root of your application.
+  Widget item(text) {
+    return GestureDetector(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xFFFFFFFF),
+          border: Border(
+            bottom: BorderSide(
+              width: 1,
+              color: Color(0xFFD9D9D9),
+            ),
+          ),
+        ),
+        height: 90,
+        child: Row(
+          children: <Widget>[
+            Container(width: 25),
+            Image.asset(
+              "assets/images/right-bracket.png",
+              width: 35,
+              height: 35,
+            ),
+            Container(width: 10),
+            Text(text, style: TextStyle(
+              fontSize: 20,
+              color: Color(0xFF707070),
+            ),),
+          ],
+        ),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        AppBarTitle(),
+        Container(
+          padding: EdgeInsets.only(left: 25, bottom: 5),
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Color(0xFFFFFFFF),
+            border: Border(
+              bottom: BorderSide(
+                width: 1.5,
+                color: Color(0xFFD9D9D9),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Color(0xFFF5F5F5),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  item("공지 사항"),
+                  item("데이터 처리 과정"),
+                ],
+              ),
+            )
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class AppBarTitle extends StatefulWidget {
+  _AppBarTitleState createState() => _AppBarTitleState();
+}
+
+class _AppBarTitleState extends State<AppBarTitle> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Text('my')
+      margin: EdgeInsets.only(top: 40.0),
+      height: 80,
+      decoration: BoxDecoration(
+        color: Color(0xFFFFFFFFF),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Align(
+            child: FlatButton(
+              onPressed: () {},
+              child: Image.asset(
+                "assets/images/left_arrow.png",
+                width: 35,
+                height: 35,
+              ),
+            ),
+          ),
+          Align(
+            child: Text('마이 페이지',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF707070))),
+          ),
+          Align(
+            child: FlatButton(),
+          ),
+        ],
+      ),
     );
   }
 }
