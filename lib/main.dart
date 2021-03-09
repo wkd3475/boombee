da803141d429e8e6231d59461796816223916678
wkd3475 <wkd3475@gmail.com>
2021-03-09 13:25:08 +0900

    update : bottom navigator 커스텀 적용

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,8 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   var _currentIndex = 0;
+  double circleSize = 50.0;
+  double circlePaddingSize = 10.0;
 
   final List<Widget> _children = [
     HomePage(),
@@ -53,33 +55,224 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          currentIndex: _currentIndex,
-          selectedItemColor: Colors.amber,
-          unselectedItemColor: Colors.black26,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'home',
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.only(bottom: 10.0),
+        height: 100.0,
+        decoration: BoxDecoration(
+            color: Color(0xFFFFFFFF),
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(25),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.map),
-              label: 'category',
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3),
+              )
+            ]),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: _currentIndex == 0
+                  ? Container(
+                      height: circleSize,
+                      decoration: BoxDecoration(
+                        color: Color(0xCCFF9300),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 1.0,
+                          color: Color(0xCCFF9300),
+                        ),
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.all(circlePaddingSize),
+                        child: Image.asset(
+                          'assets/images/home_white.png',
+                          width: 30,
+                          height: 30,
+                        ),
+                      ),
+                    )
+                  : GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = 0;
+                        });
+                      },
+                      child: Container(
+                        height: circleSize,
+                        child: Container(
+                          padding: EdgeInsets.all(circlePaddingSize),
+                          child: Image.asset(
+                            'assets/images/home_white.png',
+                            color: Colors.black45,
+                            width: 30,
+                            height: 30,
+                          ),
+                        ),
+                      ),
+                    ),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'subscribe',
+            Expanded(
+              flex: 1,
+              child: _currentIndex == 1
+                  ? Container(
+                      height: circleSize,
+                      decoration: BoxDecoration(
+                        color: Color(0xCCFF9300),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 1.0,
+                          color: Color(0xCCFF9300),
+                        ),
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.all(circlePaddingSize),
+                        child: Image.asset(
+                          'assets/images/map_icon.png',
+                          color: Colors.white,
+                          width: 30,
+                          height: 30,
+                        ),
+                      ),
+                    )
+                  : GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = 1;
+                        });
+                      },
+                      child: Container(
+                        height: circleSize,
+                        child: Container(
+                          padding: EdgeInsets.all(circlePaddingSize),
+                          child: Image.asset(
+                            'assets/images/map_icon.png',
+                            color: Colors.black45,
+                            width: 30,
+                            height: 30,
+                          ),
+                        ),
+                      ),
+                    ),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'my',
+            Expanded(
+              flex: 1,
+              child: _currentIndex == 2
+                  ? Container(
+                      height: circleSize,
+                      decoration: BoxDecoration(
+                        color: Color(0xCCFF9300),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 1.0,
+                          color: Color(0xCCFF9300),
+                        ),
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.all(circlePaddingSize),
+                        child: Image.asset(
+                          'assets/images/heart_white_icon.png',
+                          width: 30,
+                          height: 30,
+                        ),
+                      ),
+                    )
+                  : GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = 2;
+                        });
+                      },
+                      child: Container(
+                        height: circleSize,
+                        child: Container(
+                          padding: EdgeInsets.all(circlePaddingSize),
+                          child: Image.asset(
+                            'assets/images/heart_white_icon.png',
+                            color: Colors.black45,
+                            width: 30,
+                            height: 30,
+                          ),
+                        ),
+                      ),
+                    ),
             ),
-          ]),
+            Expanded(
+              flex: 1,
+              child: _currentIndex == 3
+                  ? Container(
+                      height: circleSize,
+                      decoration: BoxDecoration(
+                        color: Color(0xCCFF9300),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 1.0,
+                          color: Color(0xCCFF9300),
+                        ),
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.all(circlePaddingSize),
+                        child: Image.asset(
+                          'assets/images/man_icon.png',
+                          width: 30,
+                          height: 30,
+                        ),
+                      ),
+                    )
+                  : GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _currentIndex = 3;
+                        });
+                      },
+                      child: Container(
+                        height: circleSize,
+                        child: Container(
+                          padding: EdgeInsets.all(circlePaddingSize),
+                          child: Image.asset(
+                            'assets/images/man_icon.png',
+                            color: Colors.black45,
+                            width: 30,
+                            height: 30,
+                          ),
+                        ),
+                      ),
+                    ),
+            ),
+          ],
+        ),
+      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //     type: BottomNavigationBarType.fixed,
+      //     onTap: (index) {
+      //       setState(() {
+      //         _currentIndex = index;
+      //       });
+      //     },
+      //     currentIndex: _currentIndex,
+      //     selectedItemColor: Colors.amber,
+      //     unselectedItemColor: Colors.black26,
+      //     items: <BottomNavigationBarItem>[
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.home),
+      //         label: '',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.map),
+      //         label: '',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.home),
+      //         label: '',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.home),
+      //         label: '',
+      //       ),
+      //     ]),
     );
   }
 }
