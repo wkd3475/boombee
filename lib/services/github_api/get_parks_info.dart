fd47d489d65b966800455c3551f054c99e5ceffc
wkd3475 <wkd3475@gmail.com>
2021-03-12 16:57:58 +0900

    update : 쓰일 가능성만 있는 기능 일단 주석처리

--- lib/services/github_api/get_parks_info.dart
+++ lib/services/github_api/get_parks_info.dart
@@ -35,18 +35,18 @@ Future<List<Park>> fetchGetParksInfoList() async {
   }
 }
 
-Future<Park> fetchGetParkInfo(String parkId) async {
-  final response =
-  await http.get('http://wjddls1771.github.io/boombee/get_park_info/$parkId.json');
-
-  if (response.statusCode == 200) {
-    // 만약 서버로의 요청이 성공하면, JSON을 파싱합니다.
-    return Park.fromJson(json.decode(response.body));
-  } else {
-    // 만약 요청이 실패하면, 에러를 던집니다.
-    throw Exception('Failed to load post');
-  }
-}
+// Future<Park> fetchGetParkInfo(String parkId) async {
+//   final response =
+//   await http.get('http://wjddls1771.github.io/boombee/get_park_info/$parkId.json');
+//
+//   if (response.statusCode == 200) {
+//     // 만약 서버로의 요청이 성공하면, JSON을 파싱합니다.
+//     return Park.fromJson(json.decode(response.body));
+//   } else {
+//     // 만약 요청이 실패하면, 에러를 던집니다.
+//     throw Exception('Failed to load post');
+//   }
+// }
 
 class ParkData {
   final String date;
